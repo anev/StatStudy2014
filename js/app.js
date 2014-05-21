36c9b19f446b6a6f99548390a651cfb392905224
Vadim09 <yakimenko09@mail.ru>
2014-05-21 12:52:56 +0400

    commit
    попробывали селекторы

--- js/app.js
+++ js/app.js
@@ -1,17 +1,36 @@
 $(document).ready(function(){
-	alert('Приложение запущено!');
+	//alert('Приложение запущено!');
 
     $("#step1").click(function() {
       alert('css селектор');
     });
 
-	$("#step2").click(function() {
-	  //
-      alert('дублирование');
+	$("#step2").click(function(){
+	 $("#copymeElement").clone()
+
+
+         .appendTo($("#copymeElement"));
+        $("#copymeElement").removeAttr("id");
+      //  alert('дублирование');
+
     });
 
     $("#step3").click(function() {
-      alert('сетевой запрос');
+        $.ajax({
+            url: "http://account.bkfonbet.com/MobileServices/ping",
+// the name of the callback parameter, as specified by the YQL service
+            jsonp: "callback",
+// tell jQuery we're expecting JSONP
+            dataType: "jsonp",
+// tell YQL what we want and that we want JSON
+
+// work with the response
+            success: function( response ) {
+                alert( response ); // server response
+            }
+        });
+
+      //alert('сетевой запрос');
     });
 
 });
