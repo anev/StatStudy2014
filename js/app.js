694dc206c7acebc4852df4b1db5850449e4598b0
Vadim09 <yakimenko09@mail.ru>
2014-05-28 21:32:55 +0400

    v5

--- js/app.js
+++ js/app.js
@@ -4,8 +4,14 @@ $(document).ready(function(){
         $("#items").empty();
         $.getJSON('items.json', function (data) {
             var user = $("#filteruser").val();
+            var datetime = $("#filterdatetime").val();
+            var action = $("#filteraction").val();
             for (var i = 0; i < data.items.length; i++) {
-                if (user.length ===0 || user === data.items[i].user) {
+                if ((user.length ===0 || user === data.items[i].user) &&
+                    (datetime.length ===0|| datetime === data.items[i].datetime) &&
+                    (action.length ===0|| action === data.items[i].action) )
+
+                {
                     $('#items').append('<tr> <td>' + data.items[i].user + '</td> <td>' + data.items[i].datetime +
                         '</td> <td>' + data.items[i].action + '</td> <tr>');
                 }
