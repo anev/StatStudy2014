5584b5105be6506dd0d41af6bf4ff2590556d043
Vadim09 <yakimenko09@mail.ru>
2014-05-30 12:56:39 +0400

    v5.1

--- js/app.js
+++ js/app.js
@@ -2,12 +2,38 @@ $(document).ready(function(){
 
     var fetchAndDraw = function() {
         $("#items").empty();
+        $("#filteraction").append('<option></option>')
+        $("#filteruser").append('<option></option>')
+        var user = $("#filteruser").val();
+        var action = $("#filteraction").val();
+        $("#filteruser").empty();
+        $("#filteraction").empty();
         $.getJSON('items.json', function (data) {
-            var user = $("#filteruser").val();
+
             var datetime = $("#filterdatetime").val();
-            var action = $("#filteraction").val();
+            $("#filteraction").append('<option></option>')
+
+            $("#filteruser").append('<option></option>')
+            for (var i = 0; i < data.items.length; i++){
+
+
+
+                if ($('#filteraction>:contains("'+data.items[i].action+'")').size()      === 0) {
+                    $("#filteraction").append('<option>'+ data.items[i].action +'</option>' )
+                }
+
+
+                if  ($('#filteruser>:contains("'+data.items[i].user+'")').size()      === 0) {
+                    $("#filteruser").append('<option>'+ data.items[i].user +'</option>' )
+                }
+
+            }
+             $("#filteruser").val(user);
+            $("#filteraction").val(action);
             for (var i = 0; i < data.items.length; i++) {
-                if ((user.length ===0 || user === data.items[i].user) &&
+
+
+               if ((user.length ===0 || user === data.items[i].user) &&
                     (datetime.length ===0|| datetime === data.items[i].datetime) &&
                     (action.length ===0|| action === data.items[i].action) )
 
