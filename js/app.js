af7aac28bc95bd6eff81337f74a823368e450cdd
Vadim09 <yakimenko09@mail.ru>
2014-06-04 19:50:03 +0400

    v6

--- js/app.js
+++ js/app.js
@@ -1,19 +1,28 @@
 $(document).ready(function(){
+    var isTimeOkey = function(mcount , itemtime){
+        var twentyMinutesBefore = new Date();
+        twentyMinutesBefore.setMinutes(twentyMinutesBefore.getMinutes()- mcount);
+        var itemDate = (new Date(itemtime))
+        console.log(twentyMinutesBefore)
+        console.log(itemDate)
+        return twentyMinutesBefore < itemDate
+    }
 
     var fetchAndDraw = function() {
-        $("#items").empty();
-        $("#filteraction").append('<option></option>')
-        $("#filteruser").append('<option></option>')
-        var user = $("#filteruser").val();
-        var action = $("#filteraction").val();
-        $("#filteruser").empty();
-        $("#filteraction").empty();
+        $("#items").empty();//очистка таблицы
+        $("#filteraction").append('<option></option>')//создание пустого поля
+        $("#filteruser").append('<option></option>')//создание пустого поля
+        $("#filterdatetime").append('<option></option>')
+        var user = $("#filteruser").val();//задаем переменную, которая отвечает за  user .
+        var action = $("#filteraction").val();//задаем переменную, которая отвечает за  action.
+        $("#filteruser").empty();//очистка user
+        $("#filteraction").empty();//очистка action
         $.getJSON('items.json', function (data) {
 
             var datetime = $("#filterdatetime").val();
-            $("#filteraction").append('<option></option>')
+            $("#filteraction").append('<option></option>')//создание пустого поля в action
 
-            $("#filteruser").append('<option></option>')
+            $("#filteruser").append('<option></option>')//создание пустого поля в user
             for (var i = 0; i < data.items.length; i++){
 
 
@@ -30,11 +39,13 @@ $(document).ready(function(){
             }
              $("#filteruser").val(user);
             $("#filteraction").val(action);
+console.log(datetime);
+
             for (var i = 0; i < data.items.length; i++) {
 
 
                if ((user.length ===0 || user === data.items[i].user) &&
-                    (datetime.length ===0|| datetime === data.items[i].datetime) &&
+                    isTimeOkey(datetime, data.items[i].datetime) &&
                     (action.length ===0|| action === data.items[i].action) )
 
                 {
@@ -48,9 +59,10 @@ $(document).ready(function(){
     }
 
     fetchAndDraw();
-    $("#filtersubmit").click(fetchAndDraw);
+    $("#filtersubmit").click(fetchAndDraw);//нажатие на кнопку.
 
     // При открытии страницы делаем запрос и строим табличку
     fillTable(fetchItems);
+
 
 });
